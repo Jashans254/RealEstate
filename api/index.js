ca3051ba55d04377944ec6ba6edf8f6623e06d8c
Geetika <pappygarg77@gmail.com>
2024-07-15 18:46:41 +0530

    api index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+.then(()=> {
+    console.log("Connected to mongodb");
+})
+.catch((err)=> {
+    console.log(err);
+});
 
 app.listen(3000,()=> {
     console.log("server os running on port 3000");
