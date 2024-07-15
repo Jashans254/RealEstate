dd4f52ccf13f5df7ead86f40e08b54fde7228f6a
Geetika <pappygarg77@gmail.com>
2024-07-15 19:15:03 +0530

    test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"
 dotenv.config();
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/test')
@@ -14,3 +15,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/test')
 app.listen(3000,()=> {
     console.log("server os running on port 3000");
 });
+
+//api route
+app.use("/api/user", userRouter);
