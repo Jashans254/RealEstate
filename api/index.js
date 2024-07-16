25c2bfb4f86f05b9e00df9739ea4e62240504b09
Geetika <pappygarg77@gmail.com>
2024-07-16 07:42:08 +0530

    signup api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
+
+app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 .then(()=> {
     console.log("Connected to mongodb");
@@ -18,3 +21,5 @@ app.listen(3000,()=> {
 
 //api route
 app.use("/api/user", userRouter);
+app.use("/api/auth" , authRouter);
+
