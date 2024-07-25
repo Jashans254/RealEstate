daf05f1d62220d870104082f212fe4385e4ddf17
Geetika <pappygarg77@gmail.com>
2024-07-25 21:34:01 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -25,3 +25,25 @@ export const deletelisting = async(req,res,next)=> {
     next(error);
   }
 }
+
+export const updatelisting = async (req,res,next)=> {
+  const listing = await Listing.findById(req.params.id);
+  if(!listing){
+    return next(errorHandler(404, 'Listing not found'));
+  }
+  if(req.user.id !== listing.userRef){
+    return next(errorHandler(401, 'You can only update your own listing!'));
+  }
+
+  try{
+      const updatelisting = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true},
+      )
+      res.status(200).json(updatelisting);
+  }catch(error){
+    next(error);
+  }
+
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createlisting , deletelisting} from '../controllers/listing.controller.js';
+import { createlisting , deletelisting, updatelisting} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post("/create", createlisting);
 router.delete("/delete/:id", verifyToken, deletelisting);
+router.post("/update/:id", verifyToken, updatelisting);
 
 export default router;
