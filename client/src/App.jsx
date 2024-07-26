7ff6a94284c371d75ae907dbc4c46dd4575e0ed7
Geetika <pappygarg77@gmail.com>
2024-07-26 18:17:07 +0530

    add contact landlord functionality to the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from  './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 
 export default function App() {
@@ -19,13 +20,14 @@ export default function App() {
       <Route path='/' element={<Home />} > </Route>
       <Route path='/sign-in' element={<SignIn/>} > </Route>
       <Route path='/sign-up' element={<SignUp />} > </Route>
-      <Route path='/about' element={ <About />} > </Route>
+      <Route path='/listing/:listingId' element={ <Listing />} > </Route>
       
       <Route  element={<PrivateRoute />} >
           <Route path='/profile' element={<Profile />} /> 
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/update-listing/:listingId' element={<UpdateListing/>}></Route>
       </Route>
+
     </Routes>
     </BrowserRouter>
   )
