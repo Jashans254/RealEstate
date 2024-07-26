a7ba041dadb17b2a86a185a0826f1c6cd010f183
Geetika <pappygarg77@gmail.com>
2024-07-26 18:43:43 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createlisting , deletelisting, updatelisting, getlisting} from '../controllers/listing.controller.js';
+import { createlisting , deletelisting, updatelisting, getlisting, getListings} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/create", createlisting);
 router.delete("/delete/:id", verifyToken, deletelisting);
 router.post("/update/:id", verifyToken, updatelisting);
 router.get('/get/:id',  getlisting);
+router.get('/get', getListings);
 
 export default router;
