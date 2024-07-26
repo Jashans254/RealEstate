2d8baf06a9316b2f31b7b91d216d7bc57204f71e
Geetika <pappygarg77@gmail.com>
2024-07-26 19:47:52 +0530

    complete search page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
   const handleSubmit =(e)=> {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('seatchTerm', searchTerm);
+    urlParams.set('searchTerm', searchTerm);
     const searchQuery = urlParams.toString();
      navigate(`/search?${searchQuery}`);
   };
