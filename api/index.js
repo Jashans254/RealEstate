a660d89e020a48a197474b0141db8cea2b39a164
Geetika <pappygarg77@gmail.com>
2024-07-28 00:17:41 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 // import path from 'path';
 dotenv.config();
@@ -34,6 +35,7 @@ mongoose.connect(process.env.MONGO_URI, {
     console.error("Failed to connect to MongoDB", err);
 });
 
+const  __dirname = path.resolve();
 
 app.listen(3000,()=> {
     console.log("server is running on port 3000");
@@ -43,6 +45,13 @@ app.listen(3000,()=> {
 app.use("/api/user", userRouter);
 app.use("/api/auth" , authRouter);
 app.use("/api/listing" , listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, 'client', 'dist' , 'index.html'));
+})
+
 //Middleware for error handling
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
