9651859443222afe67d7ed2decce5dbeefa9a37c
Geetika <pappygarg77@gmail.com>
2024-07-28 11:43:29 +0530

    changed

--- api/index.js
+++ api/index.js
@@ -14,19 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// mongoose.connect('mongodb://127.0.0.1:27017/test')
-// .then(()=> {
-//     console.log("Connected to mongodb");
-// })
-// .catch((err)=> {
-//     console.log(err);
-// });
 
-const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/test';
+
+const mongoURI = process.env.MONGO_URI ;
 
 mongoose.connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    
 })
 .then(() => {
     console.log("Connected to MongoDB");
